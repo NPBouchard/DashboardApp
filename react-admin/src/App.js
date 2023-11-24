fe25599bd4c9d3ee0cbb92c5ec85dec0738a64d5
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2023-11-24 12:02:52 -0500

    initial commit

---
+++ react-admin/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+
+
+  return (
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className='app'>
+        <main className='content'>
+          in main 
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+    
+  )
+}
+
+export default App
